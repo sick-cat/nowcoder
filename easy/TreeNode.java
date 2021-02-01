68526566ea6d27c7f0b2717c4b7f3bca6f5007c7
yanzhao.jia <yanzhao.jia@aelcn.com>
2021-02-01 14:04:19 +0800

    二叉树类

---
+++ easy/TreeNode.java
@@ -0,0 +1,14 @@
+/**
+ * @author JiaYanZhao
+ * @date 2021/2/1 14:01
+ */
+public class TreeNode {
+    int val = 0;
+    TreeNode left = null;
+    TreeNode right = null;
+
+    public TreeNode(int val) {
+        this.val = val;
+    }
+
+}

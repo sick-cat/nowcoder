f7d984172175f1d9adad93e1cd1d9244f280d359
yanzhao.jia <yanzhao.jia@aelcn.com>
2021-02-05 17:48:06 +0800

    二叉搜索树的第k个节点

---
+++ easy/JZ62.java
@@ -0,0 +1,43 @@
+import java.util.List;
+import java.util.Stack;
+
+/**
+ * @author JiaYanZhao
+ * @date 2021/2/5 13:45
+ * <p>
+ * 二叉搜索树的第k个节点（树）
+ * <p>
+ * 题目描述
+ * 给定一棵二叉搜索树，请找出其中的第k小的结点。
+ */
+public class JZ62 {
+    public static void main(String[] args) {
+        char[] charArr = new char[]{'5', '3', '7', '2', '4', '6', '8'};
+        TreeNode root = TreeNode.arr2Tree(charArr);
+        List<Character> characters = TreeNode.inorderTraversal(root);
+        System.out.println(characters);
+    }
+
+    TreeNode KthNode(TreeNode pRoot, int k) {
+        if (pRoot == null || k <= 0){
+            return null;
+        }
+        // 中序遍历结果
+        Stack<TreeNode> stack = new Stack<>();
+        int num = 0;
+        do {
+            if (pRoot != null) {
+                stack.push(pRoot);
+                pRoot = pRoot.left;
+            } else {
+                pRoot = stack.pop();
+                num++;
+                if (num == k){
+                    return pRoot;
+                }
+                pRoot = pRoot.right;
+            }
+        } while (pRoot != null || !stack.isEmpty());
+        return null;
+    }
+}
--- easy/TreeNode.java
+++ easy/TreeNode.java
@@ -1,3 +1,5 @@
+import java.util.*;
+
 /**
  * @author JiaYanZhao
  * @date 2021/2/1 14:01
@@ -11,4 +13,47 @@ public class TreeNode {
         this.val = val;
     }
 
+
+    /**
+     * 数组转树
+     */
+    public static TreeNode arr2Tree(char[] charArr) {
+        Queue<TreeNode> queue = new ArrayDeque<>();
+        TreeNode root = new TreeNode(charArr[0] - 48);
+        queue.offer(root);
+        int index = 1;
+        while (queue.size() > 0 && index < charArr.length) {
+            TreeNode node = queue.poll();
+            if (charArr[index] != '#') {
+                TreeNode tempNode = new TreeNode(charArr[index] - 48);
+                node.left = tempNode;
+                queue.offer(tempNode);
+            }
+            index++;
+            if (index < charArr.length && charArr[index] != '#') {
+                TreeNode tempNode = new TreeNode(charArr[index] - 48);
+                node.right = tempNode;
+                queue.offer(tempNode);
+            }
+            index++;
+        }
+        return root;
+    }
+
+    public static List<Character> inorderTraversal(TreeNode tree) {
+        // 中序遍历结果
+        List<Character> list = new ArrayList<>();
+        Stack<TreeNode> stack = new Stack<>();
+        do {
+            if (tree != null) {
+                stack.push(tree);
+                tree = tree.left;
+            } else {
+                tree = stack.pop();
+                list.add((char)(tree.val + 48));
+                tree = tree.right;
+            }
+        } while (tree != null || !stack.isEmpty());
+        return list;
+    }
 }
